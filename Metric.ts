bd9b963366d21341b7562804f89a847e8f191cb4
Grzegorz Motyl <grzegorz.motyl@stepstone.com>
2020-10-15 10:35:13 +0200

    step 3

--- Metric.ts
+++ Metric.ts
@@ -1,2 +1,15 @@
-export type Kilometer = number & { _: "Kilometer" };
-export const numberToKilometer = (n: number): Kilometer => n as Kilometer;
+export type Kilometer = {
+  _: "Kilometer";
+  value: number;
+};
+
+export const numberToKilometer = (n: number): Kilometer => {
+  if (n < 0) {
+    throw new Error("negative number is not allowed!");
+  }
+
+  return {
+    _: "Kilometer",
+    value: n,
+  };
+};
--- Orbiter.ts
+++ Orbiter.ts
@@ -5,7 +5,10 @@ import { calculateOrbit } from "./Navigator";
 let miles = numberToMile(1000);
 let kilometers = numberToKilometer(1609.344); // 1000 miles =1609.344 kilometers
 
-let iCanHackYou = -1000 as Kilometer; // error expected
+let iCanHackYou = {
+  _: "Kilometer",
+  value: -300,
+} as Kilometer;
 
 calculateOrbit(kilometers); // OK
 calculateOrbit(iCanHackYou); // iCanHackYou TS error in l.8 expected
